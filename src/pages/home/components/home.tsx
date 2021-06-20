9ff1641a229633cc550601cc836756569fe14448
Luana MedSenior <luana.alves@medsenior.com.br>
2021-06-20 00:49:30 -0300

    update

--- src/pages/home/components/home.tsx
+++ src/pages/home/components/home.tsx
@@ -1,13 +1,17 @@
-
+import { Classes } from "./style"
 interface HomeProps {
     phrase: string
-    rightWords: number
-    wrongWords: number
     acuracy: number
     wpm: number
+    entry: string
+    elapsedTime: string
+    disableForm: boolean
+
+    definirEntry: (entry: string) => void
+    startClock: () => void
 }
 
-export const Home = ({ phrase, rightWords, wrongWords, acuracy, wpm }: HomeProps) => {
+export const Home = ({ phrase, acuracy, wpm, entry, elapsedTime, disableForm, definirEntry, startClock }: HomeProps) => {
     return <>
         <div className="container mb-5 mb-5">
             <div className="row">
@@ -16,16 +20,31 @@ export const Home = ({ phrase, rightWords, wrongWords, acuracy, wpm }: HomeProps
                     <div className="border rounder p-3 mb-4">
                         {phrase}
                     </div>
-                    <div>
-                        c: {rightWords} {wrongWords} {acuracy} {wpm}
-                    </div>
                     <textarea
+                        disabled={disableForm}
                         className="form-control mb-3"
                         placeholder="Start typing..."
+                        value={entry}
+                        onChange={(event) => { definirEntry(event.target.value) }}
                     ></textarea>
-                    <div className="text-right">
-                        <button className="btn btn-light">Start Test</button>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        < div >
+                            <div>
+                                Time: {elapsedTime}
+                            </div>
+                            <div>
+                                Accuracy: {acuracy} %
+                            </div>
+                            <div>
+                                Speed: {wpm}
+                            </div>
+                        </div>
+
                     </div>
+                    <div style={Classes.buttonDiv} className="text-right">
+                        <button onClick={startClock} className="btn btn-light" >Start Test</button>
+                    </div>
+
                 </div>
             </div>
         </div>
